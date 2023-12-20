5453796efeb4c92b98b47daefe002f072c0e2b69
vikash kumar <vk1395631@gmail.com>
2023-12-20 16:12:12 +0530

    3

--- hellow.cpp
+++ hellow.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-cout<<"Hellow world aksjdf;asjdfand helow 2 with user vik "<<endl;
+cout<<"Hellow world aksjdf;asjdfand helow 3 with user vik "<<endl;
 return 0;
 }
